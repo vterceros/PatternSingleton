2fde3cf298c588da2e6f27d9d8af28cda3358398
vterceros <vh.terceros@gmail.com>
2018-03-26 17:13:27 -0400

    hello

---
+++ Log/Logger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatterSingleton.Log
+{
+    public class Logger : IDisposable
+    {
+        private static object locker = new object(); //support multi thread
+
+        private static Logger instance;
+        public static Logger Default() //Similar to MemoryCache class name .Default
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public static Logger DefaultMultiThread() //Similar to MemoryCache class name .Default
+        {
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public bool LogError(string Message)
+        {
+            Console.WriteLine(string.Format("Error message '{0}' saved",Message));
+            return true;
+        }
+
+        public void Dispose()
+        {
+            //with singleton we can implement interfaces
+            Console.WriteLine("Disposed, just to show an advantaje over just static classes");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using PatterSingleton.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatterSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Logger.Default();
+            var b = Logger.DefaultMultiThread();
+            if (a == b)
+            {
+                Console.WriteLine("same instance");
+            }
+            Logger.Default().LogError("error to log");
+            Console.ReadKey();
+        }
+    }
+}
